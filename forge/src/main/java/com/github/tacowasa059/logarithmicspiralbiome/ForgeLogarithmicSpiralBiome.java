b55c07ab6a83a4df8e8f35cc4c495d3254e0a16e
tacowasa059 <wasserstein59@gmail.com>
2026-05-06 11:32:34 +0900

    forge subproject

---
+++ forge/src/main/java/com/github/tacowasa059/logarithmicspiralbiome/ForgeLogarithmicSpiralBiome.java
@@ -0,0 +1,30 @@
+package com.github.tacowasa059.logarithmicspiralbiome;
+
+import com.github.tacowasa059.logarithmicspiralbiome.registry.SpiralBiomeSources;
+import com.github.tacowasa059.logarithmicspiralbiome.registry.SpiralFeatures;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+
+@Mod(Logarithmicspiralbiome.MODID)
+public final class ForgeLogarithmicSpiralBiome {
+    private static final DeferredRegister<MapCodec<? extends BiomeSource>> BIOME_SOURCES =
+            DeferredRegister.create(BuiltInRegistries.BIOME_SOURCE.key(), Logarithmicspiralbiome.MODID);
+
+    private static final DeferredRegister<Feature<?>> FEATURES =
+            DeferredRegister.create(BuiltInRegistries.FEATURE.key(), Logarithmicspiralbiome.MODID);
+
+    public ForgeLogarithmicSpiralBiome(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+
+        BIOME_SOURCES.register("logarithmic_spiral", () -> SpiralBiomeSources.LOGARITHMIC_SPIRAL);
+        FEATURES.register("boundary_surface", () -> SpiralFeatures.BOUNDARY_SURFACE);
+        BIOME_SOURCES.register(modBusGroup);
+        FEATURES.register(modBusGroup);
+        Logarithmicspiralbiome.init();
+    }
+}
